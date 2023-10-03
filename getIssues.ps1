96a97111a6db11214ef0479af186d4cb71bf177f
dwwatk02 <87448112+dwwatk02@users.noreply.github.com>
2023-10-03 14:11:13 -0400

    Add files via upload

--- getIssues.ps1
+++ getIssues.ps1
@@ -1,15 +1,19 @@
-write-host "======== Starting Script ========"
+write-host "======== Step: Starting Script ========"
 $aseApiKeyId = ""
 $aseApiKeySecret = ""
 $aseHostname = ""
-$appName = ""
+#$appName = "AltoroJ_Example"
 # ASE authentication
 $sessionId=$(Invoke-WebRequest -Method "POST" -Headers @{"Accept"="application/json"} -ContentType 'application/json' -Body "{`"keyId`": `"$aseApiKeyId`",`"keySecret`": `"$aseApiKeySecret`"}" -Uri "https://$aseHostname`:9443/ase/api/keylogin/apikeylogin" -SkipCertificateCheck | Select-Object -Expand Content | ConvertFrom-Json | select -ExpandProperty sessionId);
-# Looking for $aseAppName into ASE
+write-host "======== Step: Authenticating to AppScan Enterprise ========"
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
 $session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$aseHostname")));
-write-host "======== Authenticated, querying issues ========"
+#write-host $sessionId
 $headers = @{"Asc_xsrf_token"="$sessionId"}
 $headers.Add("Range","items=0-99")
-Invoke-WebRequest -WebSession $session -Headers $headers -Uri "https://$aseHostname`:9443/ase/api/issues?query=Application%20Name=$appName`&compactResponse=false" -SkipCertificateCheck -OutFile out.json -PassThru | Out-Null;
-write-host "======== Output file: out.json ========"
+write-host "======== Step: Grabbing all Applications from AppScan Enterprise ========"
+$apps = $(Invoke-WebRequest -WebSession $session -Headers $headers -Uri "https://$aseHostname`:9443/ase/api/applications?columns=name" -SkipCertificateCheck | Select-Object -Expand Content | ConvertFrom-Json | select -ExpandProperty name);
+foreach ($appName in $apps) {
+	Invoke-WebRequest -WebSession $session -Headers $headers -Uri "https://$aseHostname`:9443/ase/api/issues?query=Application%20Name=$appName`&compactResponse=false" -SkipCertificateCheck -OutFile C:\temp\$appName`.json -PassThru | Out-Null;
+	write-host "======== Step: Wrote output file for $appName` ========"
+}
